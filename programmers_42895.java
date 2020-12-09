6668048175fc144ee52abec23555237f3cad15a4
Yoon Ki Seok <yks5313@gmail.com>
2020-12-09 22:08:05 +0900

    계산식 변경
    8/10 성공
    이전 버전과 비교 후 개선필요

--- programmers_42895.java
+++ programmers_42895.java
@@ -1,134 +1,83 @@
 package tester;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Vector;
 
 public class programmers_42895 {
 
-	// 최솟값이 8보다 크면 -1을 return 합니다. or N > number
-	// 경우의수
-	// + - / * add(*10+N)
-	// 이후 now == numbe r인지 확인
+	/*
+	 * 개선방안 : map 에 n 으로 계산한 모든 결과값을 입력
+	 * map^2 만큼 사칙연산
+	 * 작은수끼리 계산하다가, 일치하면 반환, 없으면 -1
+	 */
 	public int solution(final int N, final int number) {
-		int answer = 0;
+//		int answer = -1;
 
-		if (N == number)
+		
+		if(N == number)
 			return 1;
+		
+		
+		Vector<Set<Integer>> data = new Vector<Set<Integer>>();
 
-		Queue<Integer> queue = new ArrayDeque<Integer>();
-		queue.add(N);
-
-		/*
-		 * number 에서 빼기시작, round에서
-		 * 
-		 * number - 
-		 */
-
-		/// end조건 : 8개 까지만 사용, 최소값이 나오면 변경됨
-		int end = 9;
-		for (int round = 2; round < end; round++) { // min
-			int size = queue.size();
+		
+		int[] arr = new int[9];
+		int idx = 1;
+		data.add(null); // 0번 idx 는 null
+		for (int i = 1; i <= 11111111; i = i*10 + 1) {
+			Set<Integer> set =  new HashSet<Integer>();
+			set.add(i*N);
+			data.add(set);
 			
-			for (int i = 0; i < size; i++) {
+			arr[idx] = i*N; 
+			if(i*N == number)
+				return idx;
+			idx++;
+		}
+		
+		//정렬로 사칙연산 수행
+		for (int i = 1; i < 9; i++) {
+			for (int j = 1; j <= i; j++) {
+				int use =  i + j;
+				if(use > 8)
+					break;
+				data.get(use).add(arr[i] + arr[j]);
+				data.get(use).add(arr[i] - arr[j]);
+				data.get(use).add(arr[i] * arr[j]);
 
-				int now = queue.poll();
+				if(arr[j] > 0)
+					data.get(use).add(arr[i] / arr[j]);
+				
+			}
+		}
+		
+		//결과 끼리 재연산
+		for (int i = 1; i < 8; i++) {
+			for (int j = 1; j <= i; j++) {
+				final int use = i + j;
+				if(use > 8 || i > 7)
+					continue;
+				for (int a : data.get(i)) {
+					for (int b : data.get(j)) {
+						
+						data.get(use).add(a + b);
+						data.get(use).add(a - b);
+						data.get(use).add(a*b);
 
-				if (true || now * 10 + N <= number) { // add
-					int next = now * 10 + N;
-					// end?
-					if (next == number) {
-						answer = round;	 //반드시 최소값
-						return endReturn(answer, end);
-
+						if(b > 0)
+							data.get(use).add(a/b);
 					}
-					end = checkPlus(next, round, number, N, end);
-					queue.add(next);
-				}
-
-				if (true || now * N <= number) { // *
-					int next = now * N;
-					// end?
-					if (next == number) {
-						answer = round; //반드시 최소값
-						return endReturn(answer, end);
-					}
-					end = checkPlus(next, round, number, N, end);
-					queue.add(next);
-				}
-
-				if (true || now / N <= number) { // divide
-					int next = now / N;
-					// end?
-					if (next == number) {
-						answer = round; //반드시 최소값
-						return endReturn(answer, end);
-					}
-					end = checkPlus(next, round, number, N, end);
-					queue.add(next);
-				}
-
-				if (true || now + N <= number) { // +
-					int next = now + N;
-					// end?
-					if (next == number) {
-						answer = round; //반드시 최소값
-						return endReturn(answer, end);
-					}
-					end = checkPlus(next, round, number, N, end);
-					queue.add(next);
-				}
-
-				if (true || now - N <= number) { // +
-					int next = now - N;
-					// end?
-					if (next == number) {
-						answer = round; //반드시 최소값
-						return endReturn(answer, end);
-					}
-					end = checkPlus(next, round, number, N, end);
-					queue.add(next);
 				}
 			}
-
+			
+			//결과 확인
+			if(data.get(i).contains(number))
+				return i;
 		}
-
-		return endReturn(answer, end);
-	}
-
-	private int endReturn(int answer, int end) {
-		if(answer < 1 && end > 8)
-			return -1;
-
-		if(answer > 0  && answer < end)
-			return answer;
-
-		if(end < 9)
-			return end;		
+		
 		
 		return -1;
 	}
 
-	private int checkPlus(int next, int round, final int number, final int N, int end) {
-		// N보다 작은경우
-		/**
-		 * N -1 N n-2 
-		 */
-		int newEnd = round;
-		int exist = Math.abs(number - next);
-
-		if(exist < N) { //  -(exist*2) 로 최소화가능
-				/**
-				 * 1이상 차이날경우 (N+N + ..)N 으로 표현가능
-				 * 테케 추가
-				 * 5 557 6
-				 * 5 558 7
-				 * 557 = 555 + (5+5)/5   result : 6 
-				 */
-			newEnd+=exist + 1;
-			return end < newEnd ? end : newEnd;
-		}
-		
-		return end;
-		
-	}
 }
