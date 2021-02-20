38f6f3bbd44509555b35f461efc61a15a512f950
k2seok <yks5313@gmail.com>
2021-02-20 21:46:14 +0900

    programmers lv2 - stack /queue - review-up
    https://programmers.co.kr/learn/courses/30/lessons/42584
    
    효율성 개선을 위한 idx left, right 활용: O(N^2/2)
    	 * 효율성 동일한 문제점 발생할 수 있음.
    	 * 문제 케이스 모두 처리못해서 오답으로 처리되고 있음
    
    TODO) Queue 를 활용한 문제풀이 필요

--- programmers_12904.java
+++ programmers_12904.java
@@ -1,6 +1,6 @@
 public class programmers_12904{
 //Solution
-
+//https://programmers.co.kr/learn/courses/30/lessons/12904?language=java
    public static void main(String[] args) {
 //      System.out.println(solution("zabbabzzcdcdcz"));// 7
 	   
@@ -26,7 +26,7 @@ public class programmers_12904{
 
       StringBuffer sb = new StringBuffer();
       sb.append(s);
-      if (sb.reverse().toString().equals(s)) // palin
+      if (sb.reverse().toString().equals(s)) // pelin
          return s.length();
       else {
          return Math.max(recur(s.substring(0, s.length()-1)), recur(s.substring(1, s.length())));
--- programmers_42584.java
+++ programmers_42584.java
@@ -2,25 +2,40 @@ public class programmers_42584 {
 //Solution
 
 	/*
-	 * 1) time가 len까지 ++ 되도록 구현 2) 해당 idx 값을 time까지 확인 후 ++ 처리 3) boolean end 에서 죽은
-	 * 값 true 처리 2)n 값이 len까지 ++
-	 * 
+	 * >> mk2 : 효율성 개선을 위한 idx left, right 활용: O(N^2/2)
+	 * 효율성 동일한 문제점 발생할 수 있음.
+	 * 문제 케이스 모두 처리못해서 오답으로 처리되고 있음
 	 * 
 	 */
-	// 10^5 효율성 떨어짐 
+	// 10^5 효율성 떨어짐
 	public static int[] solution(int[] prices) {
 		int[] answer = new int[prices.length];
-		boolean[] end = new boolean[prices.length];
 
-		// con : length > 1 이상
-		for (int time = 1; time < prices.length; time++) {
-			for (int idx = 0; idx < time; idx++) {
-				if (!end[idx]) {
-					answer[idx]++;
-
-					if (prices[idx] > prices[time]) // 이전값이 더 높음
-						end[idx] = true;
+		// min : length > 1 이상
+		for (int idx = 0; idx < prices.length; idx++) {
+			int nowP = prices[idx];
+			int len = prices.length;
+//			int left = 0;
+			int right = len;
+			int rightIdx = len - 1;
+			for (int leftIdx = idx; leftIdx < prices.length; leftIdx++) {
+				if (nowP > prices[leftIdx]) {
+					answer[idx] = leftIdx - idx; // 현재위치가 떨어지는 값임
+					break;
 				}
+
+				if (leftIdx > rightIdx) // right 값 모두 확인
+				{
+					if (right < len) // right 중 최소값 idx
+						answer[idx] = right - idx; //시작위치 만큼 -
+					else // 한번도 떨어진 적이 없음.
+						answer[idx] = len - 1 - idx;
+					break;
+				}
+				if (nowP > prices[rightIdx])
+					right = len - leftIdx; // right 작은값 중 최소 idx
+
+				rightIdx--;
 			}
 		}
 
@@ -30,13 +45,14 @@ public class programmers_42584 {
 
 	public static void main(String[] args) {
 
-		int f = 100000;
-		//int[] prices = new int[] { 1, 2, 3, 2, 3 };
-		int[] prices = new int[f];
+//		int[] prices = new int[100000];
 		// 100,000
-		for (int i = 0; i < 100000; i++) {
-			prices[i] = f--;
-		}
+//		int f = 100000;
+//		for (int i = 0; i < 100000; i++) {
+//			prices[i] = f--;
+//		}
+
+		int[] prices = new int[] { 1, 2, 3, 2, 3, 1 };
 		for (int n : solution(prices)) {
 			System.out.print(n + " ");
 		}
