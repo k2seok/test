ecdef1abdcb64172e3155636e5e8df17695fa7b8
k2seok <yks5313@gmail.com>
2021-02-22 22:01:07 +0900

    programmers lv2 - stack /queue - review : 10/10
        https://programmers.co.kr/learn/courses/30/lessons/42584
    
        효율성 개선을 위한 Queue 를 활용한 문제풀이
    
    con) arr 2중 for문으로도 풀이가능. 결국 설계에서 잘 처리했을경우
    	풀이가능한 문제였음.
    	mk2 설계 다시해서 풀어보기

--- programmers_42584.java
+++ programmers_42584.java
@@ -1,42 +1,48 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class programmers_42584 {
 //Solution
 
 	/*
-	 * >> mk2 : 효율성 개선을 위한 idx left, right 활용: O(N^2/2)
-	 * 효율성 동일한 문제점 발생할 수 있음.
-	 * 문제 케이스 모두 처리못해서 오답으로 처리되고 있음
+	 * >> mk2 : 효율성 개선을 위한 idx left, right 활용: O(N^2/2) 효율성 동일한 문제점 발생할 수 있음. 문제 케이스
+	 * 모두 처리못해서 오답으로 처리되고 있음
+	 * 
+	 * 
+	 * < time 만큼 실행
+	 * 
+	 * 1) now : que .add time마다  queue pop == idx
+	 * 
+	 * 2) idx <= now queue.add ans idx ++ 마지막 queue.add
 	 * 
 	 */
 	// 10^5 효율성 떨어짐
 	public static int[] solution(int[] prices) {
 		int[] answer = new int[prices.length];
 
+		Queue<Integer> queue = new ArrayDeque<Integer>();
+
 		// min : length > 1 이상
-		for (int idx = 0; idx < prices.length; idx++) {
-			int nowP = prices[idx];
-			int len = prices.length;
-//			int left = 0;
-			int right = len;
-			int rightIdx = len - 1;
-			for (int leftIdx = idx; leftIdx < prices.length; leftIdx++) {
-				if (nowP > prices[leftIdx]) {
-					answer[idx] = leftIdx - idx; // 현재위치가 떨어지는 값임
-					break;
+		for (int time = 0; time < prices.length; time++) {
+			queue.add(time); // 현재 idx 추가
+
+			int size = queue.size();
+			int nowV = prices[time];
+			for (int i = 0; i < size; i++) { // 1부터 유효
+				int idx = queue.poll();
+				if (idx == time) {
+					answer[idx] = 0; // 현재가 0 부터 시작
+				} else {
+					answer[idx]++;
 				}
 
-				if (leftIdx > rightIdx) // right 값 모두 확인
-				{
-					if (right < len) // right 중 최소값 idx
-						answer[idx] = right - idx; //시작위치 만큼 -
-					else // 한번도 떨어진 적이 없음.
-						answer[idx] = len - 1 - idx;
-					break;
-				}
-				if (nowP > prices[rightIdx])
-					right = len - leftIdx; // right 작은값 중 최소 idx
+				if (nowV >= prices[idx]) // 값이 떨어지지 않은 경우
+					queue.add(idx);
+				
+				
 
-				rightIdx--;
 			}
+
 		}
 
 		return answer;
@@ -45,16 +51,16 @@ public class programmers_42584 {
 
 	public static void main(String[] args) {
 
-//		int[] prices = new int[100000];
-		// 100,000
-//		int f = 100000;
-//		for (int i = 0; i < 100000; i++) {
-//			prices[i] = f--;
-//		}
+		int[] prices = new int[100000];
+		int f = 100000;
+		for (int i = 0; i < 100000; i++) {
+			prices[i] = f--;
+		}
 
-		int[] prices = new int[] { 1, 2, 3, 2, 3, 1 };
+//		int[] prices = new int[] { 1, 2, 3, 2, 3, 1 }; // 5 4 1 2 1 0
+//		int[] prices = new int[] { 1, 2, 3, 2, 3};
 		for (int n : solution(prices)) {
-			System.out.print(n + " ");
+			//System.out.print(n + " ");
 		}
 	}
 }
