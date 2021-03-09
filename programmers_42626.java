7adda5c8f7cd8d7cb2d2f3a0dc79008db4d71469
k2seok <yks5313@gmail.com>
2021-03-09 23:50:48 +0900

    programmers lv2 - heap, queue
        더 맵게
           https://programmers.co.kr/learn/courses/30/lessons/42626
    
         set -> priorityQueue 를 활용해서 해결
    	offer : error 시 false 리턴 (예외처리됨. add는 error로 처리)
    
    	source : https://krpeppermint100.medium.com/

--- programmers_42626.java
+++ programmers_42626.java
@@ -1,10 +1,13 @@
-import java.util.TreeSet;
+import java.util.PriorityQueue;
 
 public class programmers_42626 {
 //Solution
 
 	/**
-	 * https://programmers.co.kr/learn/courses/30/lessons/42626 goal : scoville 정렬 후
+	 * https://programmers.co.kr/learn/courses/30/lessons/42626
+	 * 
+	 * set -> priorityQueue 를 활용해서 해결
+	 * offer : 순서에 맞게 입력해준다. offer : error 시 false 리턴 (예외처리됨)
 	 * 
 	 * @param args
 	 */
@@ -13,8 +16,10 @@ public class programmers_42626 {
 //		int[] data = { 1, 2, 3, 9, 10, 12 };
 //		System.out.println(solution(data, 7));
 
-		 int[] data = {11, 0, 7, 1, 2,2,2, 3, 9, 10, 12}; // 5
-	      System.out.println(solution(data, 11));
+//		 int[] data = {11, 0, 7, 1, 2,2,2, 3, 9, 10, 12}; // k : 11 .. answer is  5
+//		 int[] data = {8,10, 11}; // K : 7 .. answer is 0
+		 int[] data = {1,1, 100}; // K : 7 .. answer is 0
+	      System.out.println(solution(data, 100));
 
 		// maximum
 		// 1,000,000
@@ -31,26 +36,27 @@ public class programmers_42626 {
 	public static int solution(int[] scoville, int k) {
 		int answer = 0;
 
-		TreeSet<Integer> set = new TreeSet<Integer>();
+		PriorityQueue<Integer> set = new PriorityQueue<>();
 
 		for (Integer i : scoville) {
-			set.add(i);
+			set.offer(i); // insert priority Idx
 		}
 
-		while (set.lower(k) != null && set.size() >= 2) {
-			Integer a = set.pollFirst();
-			Integer b = set.pollFirst();
-
-			if (a != null && b != null) {
-				set.add(a + b * 2);
-				answer++;
+		while (set.peek() < k) { // 가장 작은 값이 k 보다 큰지 확인
+			if (set.size() < 2) {
+				answer = -1;
+				break;
 			}
-		}
 
-		if (set.higher(k) == null || set.higher(k) < k)
-			answer = -1;
+			int a = set.poll();
+			int b = set.poll();
+
+			set.offer(a + b * 2);
+			answer++;
+		}
 
 		return answer;
 
 	}
+	
 }
