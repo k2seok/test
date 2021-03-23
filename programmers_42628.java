998c42186ad4c6876d5483d80609e013bf612f0c
k2seok <yks5313@gmail.com>
2021-03-23 23:02:50 +0900

    programmers lv3 - heap
        https://programmers.co.kr/learn/courses/30/lessons/42628
    
        linked Queue 로 구현
    
        Think) 직접 priorityQ 만들어보기

--- programmers_42628.java
+++ programmers_42628.java
@@ -1,124 +1,74 @@
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.PriorityQueue;
 
 /**
  * 
  * https://programmers.co.kr/learn/courses/30/lessons/42628
  * 
- * priorityQ 는 insert 시마다 정렬됨.
- * LinkedList 를 만들고, Delete 시에만 정렬(insert 된 값이 없으면 정렬하지 않음) 으로 처리하면 더 효율적
+ * priorityQ 는 insert 시마다 정렬됨. LinkedList 를 만들고, Delete 시에만 정렬(insert 된 값이 없으면
+ * 정렬하지 않음) 으로 처리하면 더 효율적
  * 
  * 
  */
 public class programmers_42628 {
 //Solution
-
 	public static int[] solution(String[] operations) {
-		class lQ {
-			int i;
-
-			lQ next;
-			lQ prev;
-			
-			
-			
-
-			public lQ(int i) {
-				this.i = i;
-			}
-
-			public void add(lQ next) {
-				this.next = next;
-				next.prev = this;
-			}
-
-			public lQ next() {
-				return this.next;
-			}
-
-			public lQ prev() {
-				return this.prev;
-			}
-
-			public int compare(lQ next) {
-				if (next == null)
-					return 0; // can added
-				else if (next.i > this.i)
-					return 1;
-				else if (next.i <= this.i)
-					return -1;
-
-				return 0;
-			}
-		}
-
 		int[] answer = {};
 
-		lQ lq = null; // always min
-
-		for (String oper : operations) {
-			String[] arr = oper.split(" ");
-			String sep = arr[0];
-			Integer i = Integer.parseInt(arr[1]);
-			if ("I".equals(sep)) {
-				lQ now = new lQ(i); // 입력할 값
-				if (lq == null)
-					lq = now; // 비어있는 경우 입력
-				else {
-					lQ idx = lq;
-
-					while (idx != null) {
-						if (idx.compare(now) > 0) {
-							if (idx.next == null) {
-								// now가 최고값
-								idx.add(new lQ(i));
-								break;
-							} else
-								idx = idx.next;
-						} else { // 현재 지점보다 now가 작으로, idx-1 now idx 로 연결
-							if (idx.prev != null) {
-								now.next = idx;
-								now.prev = idx.prev;
-								idx.prev.next = now;
-								idx.prev = now;
-							} else {
-								idx.prev = now;
-								now.next = idx;
-							}
-							break;
-						}
-					}
-
-//					for (lQ now = lq; lq.next() != null; lq = lq.next()) {
-//					}
-				}
-
-			} else if ("D".equals(sep)) {// "D"
-//				if(minQ.size() < 1 )
-//					continue;
-//				
-//				int v = 0; 
-//				if(i > 0) {
-//					v = minQ.poll();
-//					maxQ.
-//				}else {
-//					
-//				}
-//				
-//				int a =  i > 0 ? pQ.poll() : pQ.;
+		Comparator<Integer> asc = new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o1 - o2;
 			}
+		};
+		
+		List<Integer> list = new LinkedList<Integer>();
+		boolean isChange = false;
+		for (String oper : operations) {
+			String sep = oper.split(" ")[0];
+			int i = Integer.parseInt(oper.split(" ")[1]);
 
+			if("I".equals(sep)) {// "I"
+				list.add(i);
+				isChange = true;
+			} else if ("D".equals(sep)) {// "D"
+				if(isChange) {
+					list.sort(asc);
+					isChange = false;
+				}
+				
+				int dum = 0;
+				if(list.size() >0)
+					dum = i > 0 ? list.remove(list.size()-1): list.remove(0);
+
+			}
 		}
+		
+		if(list.size() > 0) {
+			list.sort(asc);
+			answer = new int[] {list.get(list.size()-1), list.get(0)};
+		}else {
+			answer = new int[] {0,0};
+		}
+		
+
 
 		return answer;
 	}
 
 	public static void main(String args[]) {
 
-//		String[] oper = new String[] { "I 16", "D 1" };
-		String[] oper = new String[] { "I 1", "I 6", "I 5", "I 16", "I 1" };
-		System.out.println(solution(oper));
+//		String[] oper = new String[] { "I 16", "D 1" }; // 0 0
+//		String[] oper = new String[] { "I 1", "I 6", "I 5", "I 16", "I 1" };
+//		String[] oper = new String[] { "I 1", "I 6", "D -1", "D -1", "I 1" };
+		String[] oper = new String[] {"I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"}
+;
+		
+
+		System.out.println(Arrays.toString(solution(oper)));
 
 	}
 }
