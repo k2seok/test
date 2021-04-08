a5368cac5f66689f7c17808d26118909bbecad79
k2seok <yks5313@gmail.com>
2021-04-08 23:56:08 +0900

    greedy - work
    https://programmers.co.kr/learn/courses/30/lessons/42861
    
    archi)
      v 만큼의 자를때.. 자른결과값 r < M 인경우
      더 잘라봐야함 : v--  (r이 더커짐)
      r >= M 인 경우 더 적게 잘라봐야함 : v++

---
+++ programmers_42861.java
@@ -0,0 +1,19 @@
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class programmers_42861 {
+//Solution
+	public static void main(String[] args) {
+		int[][] costs;
+		int n;
+		System.out.println(solution(n, costs));
+
+	}
+
+	public static int solution(int n, int[][] costs) {
+		int answer = 0;
+		return answer;˙
+	}
+
+}
