b83d09ef07fa88375670f6458aff8d87a08fdb70
k2seok <yks5313@gmail.com>
2021-04-13 00:33:01 +0900

    CommitLog : 결과 > 효율성 검증필요
    	archi) 점화식(DP) 로 가장 효율적인 2명 찾기
       max[-1]부터
    
    	    + 남은 값들 DP 식으로 계산

--- programmers_42885.java
+++ programmers_42885.java
@@ -3,37 +3,13 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 public class programmers_42885 {
-
+//Solution
+	
 	/**
 	1) 오름차순 정렬
 	2) limit 중간값 나눔
 	3) 하나씩 greedy 입력 
-	    max[-1]부터
-	    
-	    + 남은 값들 DP 식으로 계산
-	    
-	    int now = limit - max[i];
-	    i -1 한 값을 입력가능? 입려 후 종료
-	
-	    i -1 : 최대값
-	    0 : 최소값
-	    
-	    i - n
-	    
-	    do{
-	    
-	    }
-	    while(now/2 > min){
-	     int n = now/2
-	     // 나누고도 더 나눌 수 있으면(n이 작으면) 한번 더 나눔
-	     if(n < now)
-	     // N = n;
-	     else{ // n이 더 큰경우
-	     
-	     }
-	    }
-	    
-	    min[-1] 
+	 
 	    
 	
 	**/
