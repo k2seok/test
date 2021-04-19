56a368a89d87407300dcba8220446585fab16374
k2seok <yks5313@gmail.com>
2021-04-19 23:51:46 +0900

    programmers lv2 greedy, DP, work
    https://programmers.co.kr/learn/courses/30/lessons/42885

--- programmers_42885.java
+++ programmers_42885.java
@@ -13,32 +13,29 @@ public class programmers_42885 {
 	    
 	
 	**/
+	static  int[] arr;
 	static public int solution(int[] people, int limit) {
 		int answer = 0;
 
 		int count = 0;
-
-		Arrays.sort(people);
-
+		arr = people.clone();
+		
 			for (int i = people.length -1; i >= 0; i--) {
-				
+				Arrays.sort(people); // -1 갱신
+
 				if(people[i] < 1) continue; // use
 				int a = people[i];
-				boolean find = false; 
-				for (int j = i -1 ; !find && j >= 0; j--) {
-					if(people[j] < 1) continue; //use
-					int b = people[j];
-					
-					if(a + b <= limit) { //find
-						answer++;
-						count +=2;
-						find = true;
-						people[i] = -1;
-						people[j] = -1;
-					}
-				}
+				// findCal() : DP
 				
-				if(!find) {
+				int idx = findCal(count, i-1, limit - a);
+				
+				if(idx >= 0) {
+					answer++;
+					count +=2;
+					people[i] = -1;
+					people[idx] = -1;
+				
+				}else if(idx <= -1) {
 					answer++;
 					people[i] = -1;
 					count++;
@@ -47,6 +44,24 @@ public class programmers_42885 {
 		}
 
 		return answer;
+	}
+
+	private static int findCal(int start, int end, int v) {
+		if(start < 0 || end < 0)
+			return -1;
+		
+		int idx = (start + end)/2;
+		
+		if(v > arr[idx]) {
+			return findCal(idx + 1, end, v);
+		}else if(v < arr[idx]) {
+			return findCal(start, idx -1, v);
+		}
+		else if(v == arr[idx]){ 
+			return idx;
+		}
+		
+		return -1; //not Find;
 	}
 
 	static public void main(String args[]) {
