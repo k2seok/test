8ca3d981ca2bc1605a5a76fab89e8db5e71b893b
k2seok <yks5313@gmail.com>
2021-05-12 01:02:57 +0900

    programmers String,regex
        https://programmers.co.kr/learn/courses/30/lessons/72410
    
        con) 정규식 공부, 구글링 참고해서 구현

---
+++ programmers_72410.java
@@ -0,0 +1,44 @@
+public class programmers_72410 {
+
+	public static void main(String[] args) {
+
+		System.out.println(solution("...!@BaT#*..y.abcdefghijklm."	));
+		System.out.println(solution("=.="	));
+		
+	}
+
+	
+	/**
+	 * regex�� Ȱ���� ���ڿ� ��ȯ
+	 * rule 7�� ����  ����
+	 * @param new_id
+	 * @return
+	 */
+	  public static String solution(String new_id) {
+	        String answer = new_id;
+	        
+	        //1
+	        answer = answer.toLowerCase();
+//	        System.out.println(answer);
+	        //2
+	        answer = answer.replaceAll("[^a-z0-9\\-\\_\\.]", "");
+	        //3
+	        answer = answer.replaceAll("\\.{2,}", ".");
+//	        System.out.println(answer);
+	        
+	        //4
+	        answer = answer.replaceAll("^\\.|\\.$", "");
+	        //5
+	        answer = answer.length() < 1 ? "a" : answer;
+//	        System.out.println(answer);
+	        //6
+	        answer = answer.length() < 15 ? answer : answer.substring(0, 15).replaceAll("\\.$", "");
+	        //7
+	        String e= (""+answer.charAt(answer.length()-1)); 
+	        answer = answer.length() > 2 ? answer : (answer+ e + e+ e).substring(0,3); 
+//	        System.out.println(answer);
+//	        
+	        
+	        return answer;
+	    }
+}
